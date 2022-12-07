181095425523e11f667ed4106288115ae16defd6
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-07 17:24:06 +0000

    Added grid and styling for main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import EditSection from "./EditSection";
+import Button from "./Button";
+
+const Main = () => {
+  return (
+    <div className="grid grid-cols-2 max-w-7xl mx-auto mt-2 p-7">
+      <div className="bg-yellow-200 w-full p-5">
+        <EditSection />
+        <EditSection />
+        <EditSection />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="bg-yellow-400 w-full p-5">Main</div>
+    </div>
+  );
+};
+
+export default Main;
