71eb2ddd323501d00406bcc585415327d57f2fbd
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-07 17:05:39 +0000

    Added header + styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="flex justify-center bg-gray-600">
+      <h1 className="font-zen text-5xl p-5 text-gray-300">CV CREATOR</h1>
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: "'Raleway', sans-serif",
+        zen: "'Zen Tokyo Zoo', cursive",
+      },
+    },
   },
   plugins: [],
 };
