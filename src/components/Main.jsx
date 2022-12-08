da6e70f6f9b069ecc1aec9fbc27610def307a308
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-08 17:51:40 +0000

    Added css and components for main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,13 @@
 import CVForm from "./CVForm";
+import Buttons from "./Buttons";
 
 const Main = () => {
   return (
     <div className="grid grid-cols-2 gap-12 max-w-7xl mx-auto mt-2 p-7 font-raleway">
-      <div className="bg-neutral-200 w-full p-5 rounded">
+      <div className="bg-zinc-200 w-full p-5 rounded shadow-2xl">
         <CVForm />
-        <Button />
-        <Button />
-        <Button />
       </div>
-      <div className="bg-yellow-400 w-full p-5 rounded">Main</div>
+      <div className="bg-zinc-200 w-full p-5 rounded shadow-xl">Main</div>
     </div>
   );
 };
