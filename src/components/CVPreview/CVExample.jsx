ec80d3e6cf9ef190f280598f21a970369e7e53c0
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-10 13:59:00 +0000

    Coded header section of example using props

--- src/components/CVPreview/CVExample.jsx
+++ src/components/CVPreview/CVExample.jsx
@@ -1,14 +1,9 @@
-import placeholder from "../images/placeholder.png";
+import CVHeader from "./CVHeader";
 
 const CVExample = ({ cv }) => {
   return (
     <div className="grid grid-cols-[2.5fr_1fr] grid-rows-[1fr_7.5fr] sticky top-2.5 self-start bg-zinc-100 w-full shadow-2xl">
-      <header className="p-5 bg-blue-800 text-zinc-100 col-span-2">
-        <h1 className="text-5xl font-semibold">
-          {cv.personalInfo.firstName} {cv.personalInfo.lastName}
-        </h1>
-        <p className="text-xl mt-1">{cv.personalInfo.title}</p>
-      </header>
+      <CVHeader personalInfo={cv.personalInfo} />
       <div className="p-5 bg-white">
         <section className="flex flex-col gap-4 mb-8">
           <h3 className="text-lg font-semibold text-blue-800 border-b">
@@ -22,11 +17,7 @@ const CVExample = ({ cv }) => {
           <h3 className="text-lg font-semibold text-blue-800 border-b">
             Experience
           </h3>
-          <div className="grid grid-cols-[1fr_1fr] gap-4">
-            {cv.experiences.map((experience) => (<>
-              <div key={experience.id} className="font-semibold"></div>
-              <div className="flex flex-col gap-4"></div></>)}
-          </div>
+          <div className="grid grid-cols-[1fr_1fr] gap-4"></div>
         </section>
         <section></section>
       </div>
---
+++ src/components/CVPreview/CVHeader.jsx
@@ -0,0 +1,12 @@
+const CVHeader = ({ personalInfo: { firstName, lastName, title } }) => {
+  return (
+    <header className="p-5 bg-blue-800 text-zinc-100 col-span-2">
+      <h1 className="text-5xl font-semibold">
+        {firstName} {lastName}
+      </h1>
+      <p className="text-xl mt-1">{title}</p>
+    </header>
+  );
+};
+
+export default CVHeader;
