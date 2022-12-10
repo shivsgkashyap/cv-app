22747e1cb31876f5cd91982c7230d29d7e7a93c8
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-10 13:52:18 +0000

    Added state for initial CV and passed as props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
+import CVExample from "./CVExample";
 import CVForm from "./CVForm";
-import Buttons from "./Buttons";
+import emptyCV from "./Utils/emptyCV";
+import exampleCV from "./Utils/exampleCV";
 
 const Main = () => {
+  const [cv, setCV] = useState(exampleCV);
+
   return (
     <main className="grid grid-cols-2 gap-12 max-w-7xl mx-auto mt-2 p-7 font-raleway">
-      <section className="bg-zinc-100 w-full p-5 rounded shadow-2xl">
-        <CVForm />
-      </section>
-      <section className="bg-zinc-100 w-full p-5 rounded shadow-xl">
-        Main
-      </section>
+      <CVForm cv={cv} />
+      <CVExample cv={cv} />
     </main>
   );
 };
